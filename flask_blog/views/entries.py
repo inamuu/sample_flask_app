b26b1515289949add7de72cb3b58f54e1f0c0160
inamuu <8310973+inamuu@users.noreply.github.com>
2020-08-28 23:50:01 +0900

    :+1: create entry form

--- flask_blog/views/entries.py
+++ flask_blog/views/entries.py
@@ -6,3 +6,9 @@ def show_entries():
     if not session.get('logged_in'):
         return redirect(url_for('login'))
     return render_template('entries/index.html')
+
+@app.route("/new_entry")
+def new_entry():
+    if not session.get('logged_in'):
+        return redirect(url_for('new_entry'))
+    return render_template('entries/new.html')
