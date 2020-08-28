4aa3043e5fbc6d16e5a61f4df1293743d6dd0e90
inamuu <8310973+inamuu@users.noreply.github.com>
2020-08-29 00:01:40 +0900

    :+1: add entry method

--- flask_blog/views/entries.py
+++ flask_blog/views/entries.py
@@ -1,5 +1,6 @@
 from flask import request, redirect, url_for, render_template, flash, session
 from flask_blog import app
+from flask_blog import db
 
 @app.route("/")
 def show_entries():
@@ -12,3 +13,18 @@ def new_entry():
     if not session.get('logged_in'):
         return redirect(url_for('new_entry'))
     return render_template('entries/new.html')
+
+@app.route("/entries", methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+
+    entry = Entry(
+        title=request.form['title'],
+        text=request.form['text']
+    )
+
+    db.session.add(entry)
+    db.session.commit()
+    flash('新しい記事が投稿されました')
+    return redirect(url_for('show_entries'))
