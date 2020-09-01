7cf202abca79d6b00627d6d82d422c0c5c944d53
inamuu <8310973+inamuu@users.noreply.github.com>
2020-09-02 00:29:08 +0900

    :hammer: divide view files

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -1,36 +1,41 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config.from_object('flask_blog.config')
+db = SQLAlchemy()
 
-db = SQLAlchemy(app)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('flask_blog.config')
+    db.init_app(app)
 
-from flask_blog.views.views import view
-app.register_blueprint(view)
 
-from flask_blog.views.entries import entry
-app.register_blueprint(entry, url_prefix='/users')
+    from flask_blog.views.views import view
+    app.register_blueprint(view)
+
+    from flask_blog.views.entries import entry
+    app.register_blueprint(entry, url_prefix='/users')
+
+    return app
 
 
 """
 modelsディレクトリ配下が読まれないので暫定対処
 """
-from flask_blog import db
-from datetime import datetime
-
-class Entry(db.Model):
-    __tablename__ = 'entries'
-    __table_args__ = {'extend_existing': True}
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(50), unique=True)
-    text = db.Column(db.Text)
-    created_at = db.Column(db.DateTime)
-
-    def __init__(self, title=None, text=None):
-        self.title = title
-        self.text = text
-        self.created_at = datetime.utcnow()
-
-    def __repr__(self):
-        return '<Entry id:{} title:{} text:{}>'.format(self.id, self.title, self.text)
+#from flask_blog import db
+#from datetime import datetime
+#
+#class Entry(db.Model):
+#    __tablename__ = 'entries'
+#    __table_args__ = {'extend_existing': True}
+#    id = db.Column(db.Integer, primary_key=True)
+#    title = db.Column(db.String(50), unique=True)
+#    text = db.Column(db.Text)
+#    created_at = db.Column(db.DateTime)
+#
+#    def __init__(self, title=None, text=None):
+#        self.title = title
+#        self.text = text
+#        self.created_at = datetime.utcnow()
+#
+#    def __repr__(self):
+#        return '<Entry id:{} title:{} text:{}>'.format(self.id, self.title, self.text)
