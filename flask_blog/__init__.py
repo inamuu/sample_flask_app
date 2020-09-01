bf43cca3e440279009586365e76ff046368e20d8
inamuu <8310973+inamuu@users.noreply.github.com>
2020-09-02 00:06:02 +0900

    :hammer: divide views using blueprint

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -6,10 +6,12 @@ app.config.from_object('flask_blog.config')
 
 db = SQLAlchemy(app)
 
+from flask_blog.views.views import view
+app.register_blueprint(view)
+
 from flask_blog.views.entries import entry
 app.register_blueprint(entry, url_prefix='/users')
 
-from flask_blog.views import views, entries
 
 """
 modelsディレクトリ配下が読まれないので暫定対処
